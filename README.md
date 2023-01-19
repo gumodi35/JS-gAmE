dc35d6f49d987367cb15d0d6a83d0ad343f65f1d
gumodi10@gmail.com <gumodi10@gmail.com>
2023-01-18 21:09:41 -0600

    Add emojis

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
 # Vamos a trabajar con Canvas
 ## Pero ¿que es Canvas?
 Canvas de JavaScript es una característica de HTML5 que permite dibujar gráficos y animaciones en una página web utilizando JavaScript. Es una herramienta poderosa que permite a los desarrolladores crear juegos, visualizaciones de datos y otras aplicaciones interactivas en el navegador. Al utilizar JavaScript, se puede aprovechar toda la potencia de la programación para crear gráficos y animaciones complejos.
+
+>Nuestro Juego se vera asi:
+
+![Game]("./images/")
--- game.js
+++ game.js
@@ -2,15 +2,49 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
+//funcion que se ejecuta cuando se cambia el tamaño de la ventana
+/* window.addEventListener("resize", function () {
+  game.width = window.innerWidth;
+  game.height = window.innerHeight;
+}); */
+
 // agregamos el objeto window para cargar nuestra funcion
-window.addEventListener('load', iniciarJuego)
+window.addEventListener("load", iniciarJuego);
 
 // creamos una funcion para inicializar el juego
 function iniciarJuego() {
-    // game.fillRect(50,50,50,50)
+  // variables para definir el tamaño del canvas
+  let tamañoCanvas;
+
+  // codicional para redimensionar canvas
+/*   if (window.innerHeight > window.innerWidth) {
+    tamañoCanvas = window.innerWidth * 0.8;
+  } else {
+    tamañoCanvas = window.innerHeight * 0.8;
+  }
+   */
+  // usando operador ternario
+  tamañoCanvas = window.innerHeight > window.innerWidth 
+  ? window.innerWidth * .80 
+  : window.innerHeight * .80;
+
+  canvas.setAttribute('width', tamañoCanvas);
+  canvas.setAttribute('height', tamañoCanvas);
+  
+  const elementsTamaño = tamañoCanvas / 10;
+
+  console.log({tamañoCanvas, elementsTamaño})
+
+  game.font = elementsTamaño + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsTamaño, elementsTamaño * i);
+  }
+
+  /*   game.fillRect(50,50,50,50)
     game.font= "25px Monserrat";
     game.fillStyle = "blue";
     game.textAlign = "left";
-    game.fillText('gual', 50,50)
+    game.fillText('gual', 50,50) */
 }
-
