e08e1037be157b998cc16c131dbaf68cabeb1890
gumodi10@gmail.com <gumodi10@gmail.com>
2023-01-23 19:58:17 -0600

    Refactor code

--- game.js
+++ game.js
@@ -1,66 +1,17 @@
-/*
-// definimos nuestra variables
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
-
-//funcion que se ejecuta cuando se cambia el tamaño de la ventana
- window.addEventListener("resize", function () {
-  game.width = window.innerWidth;
-  game.height = window.innerHeight;
-}); 
-
-// agregamos el objeto window para cargar nuestra funcion
-window.addEventListener("load", iniciarJuego);
-window.addEventListener("resize", iniciarJuego);
-
-// creamos una funcion para inicializar el juego
-function iniciarJuego() {
-  // variables para definir el tamaño del canvas
-  let tamañoCanvas;
-
-  // codicional para redimensionar canvas
- if (window.innerHeight > window.innerWidth) {
-    tamañoCanvas = window.innerWidth * 0.8;
-  } else {
-    tamañoCanvas = window.innerHeight * 0.8;
-  }
-   
-  // usando operador ternario
-  tamañoCanvas = window.innerHeight > window.innerWidth 
-  ? window.innerWidth * .80 
-  : window.innerHeight * .80;
-
-  canvas.setAttribute('width', tamañoCanvas);
-  canvas.setAttribute('height', tamañoCanvas);
-  
-  const elementsTamaño = (tamañoCanvas / 10) - 1;
-
-  console.log({tamañoCanvas, elementsTamaño})
-
-  game.font = elementsTamaño + 'px Verdana';
-  game.textAlign = 'end';
-
-  for (let i = 0; i <= 10; i++) {
-    for(let z = 1; z < 11; z++){
-      game.fillText(emojis['X'], elementsTamaño * i, elementsTamaño * z);
-    }
-  }
-
-     game.fillRect(50,50,50,50)
-    game.font= "25px Monserrat";
-    game.fillStyle = "blue";
-    game.textAlign = "left";
-    game.fillText('gual', 50,50) 
-}
-*/
-
-// checar este codigo para acomodar el mio
-
-const canvas = document.querySelector("#game");
-const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -80,49 +31,83 @@ function setCanvasSize() {
   startGame();
 }
 
-const buttons = document.querySelectorAll(".btns button");
-buttons.forEach(function (button) {
-  button.addEventListener("click", handleButtonClick);
-});
-
-function handleButtonClick(event) {
-  const button = event.target;
-  console.log(`El botón ${button.id} fue presionado`);
-}
-
-const flechas = window.addEventListener("keydown", handleKeyDown);
-
-function handleKeyDown(event) {
-  event.preventDefault();
-  if (event.key === "ArrowUp") {
-    console.log("La tecla de flecha hacia arriba fue presionada");
-  } else if (event.key === "ArrowDown") {
-    console.log("La tecla de flecha hacia abajo fue presionada");
-  } else if (event.key === "ArrowLeft") {
-    console.log("La tecla de flecha hacia izquierda fue presionada");
-  } else if (event.key === "ArrowRight") {
-    console.log("La tecla de flecha hacia derecha fue presionada");
-  }
-}
-
 function startGame() {
   console.log({ canvasSize, elementsSize });
 
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  const map = maps[1];
-  const mapRows = maps[0].trim().split("\n");
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
   const mapRowCols = mapRows.map((row) => row.trim().split(""));
-  console.log(map, mapRows, mapRowCols);
+  console.log({ map, mapRows, mapRowCols });
 
-  for (row = 1; row <= 10; row++) {
-    for (col = 1; col <= 10; col++) {
-      game.fillText(
-        emojis[mapRowCols[row - 1][col - 1]],
-        elementsSize * col,
-        elementsSize * row
-      );
-    }
+  game.clearRect(0, 0, canvasSize, canvasSize);
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+
+      if (col == "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+          console.log({ playerPosition });
+        }
+      }
+
+      game.fillText(emoji, posX, posY);
+    });
+  });
+
+  movePlayer();
+}
+
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(event) {
+  if (event.key == "ArrowUp") moveUp();
+  else if (event.key == "ArrowLeft") moveLeft();
+  else if (event.key == "ArrowRight") moveRight();
+  else if (event.key == "ArrowDown") moveDown();
+}
+
+function moveUp() {
+  if (playerPosition.y - elementsSize > 0) {
+    playerPosition.y -= elementsSize;
+  }
+  console.log("Me quiero mover hacia arriba");
+  startGame();
+}
+function moveLeft() {
+  if (playerPosition.x - elementsSize > elementsSize) {
+    playerPosition.x -= elementsSize;
+    console.log("Me quiero mover hacia la izquierda");
+    startGame();
+  }
+}
+
+function moveRight() {
+  if (playerPosition.x + elementsSize < canvasSize + elementsSize) {
+    playerPosition.x += elementsSize;
+    console.log("Me quiero mover hacia la derecha");
+    startGame();
+  }
+}
+
+function moveDown() {
+  if (playerPosition.y + elementsSize < canvasSize + elementsSize) {
+    playerPosition.y += elementsSize;
+    console.log("Me quiero mover hacia abajo");
+    startGame();
   }
 }
